9166c704d778d6c639a843a9729a9cdb6eb12123
Nhut-Nam Le <lenam.fithcmus@gmail.com>
2020-11-21 17:36:09 +0700

    test2

---
+++ test/test2/query.pl
@@ -0,0 +1,10 @@
+giang_vien('LeNgocThanh').
+bo_mon_cung_khoa('Khoahocmaytinh',X).
+truong_khoa('DinhBaTien','CNTT').
+nganh_cung_khoa('Khoahocmaytinh','Kythuatphanmem').
+cung_la_giao_su('LeHoaiBac','LyQuocNgoc').
+truong_bo_mon(X,'Khoahocmaytinh').
+pho_bo_mon(X,'Khoahocmaytinh').
+chuyen_nganh_cung_nganh('Congnghetrithuc','VatlyLythuyet').
+phong('Khoahocmaytinh','I.81').
+hoc_ham(X, 'GS').
